7ed805b51c2c041dd18899bfe623e92223bb2a58
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-04-02 13:00:44 -0700

    Add version option

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,31 @@
 #include <getopt.h>
+#include <iostream>
 
 #include "game.hh"
 
-#define optswitch        \
-    case 't':            \
-        tutorial = true; \
-        break;           \
-    case 'C':            \
-        color = false;   \
+#define VMAJOR 1
+#define VMINOR 0
+
+#define optswitch                                               \
+    case 't':                                                   \
+        tutorial = true;                                        \
+        break;                                                  \
+    case 'C':                                                   \
+        color = false;                                          \
+        break;                                                  \
+    case 'v':                                                   \
+        std::cout << "cg v" << VMAJOR << '.' << VMINOR << '\n'; \
+        std::exit(0);                                           \
         break;
 
 int main(int argc, char **argv) {
     bool tutorial = false;
     bool color = true;
     struct option longOptions[] = {{"tutorial", no_argument, 0, 't'},
-                                   {"nocolor", no_argument, 0, 'C'}};
+                                   {"nocolor", no_argument, 0, 'C'},
+                                   {"version", no_argument, 0, 'v'}};
     int optidx, opt;
-    while ((opt = getopt_long(argc, argv, "tC", longOptions, &optidx)) != -1) {
+    while ((opt = getopt_long(argc, argv, "tCv", longOptions, &optidx)) != -1) {
         switch (opt) {
             case 0:
                 if (longOptions[optidx].flag != 0) break;
